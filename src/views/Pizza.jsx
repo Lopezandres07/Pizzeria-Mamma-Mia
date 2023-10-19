53e54608de78c3240d92f85dd4ad1109ee9de745
Andrés López <andlop17@gmail.com>
2023-10-18 21:09:03 -0300

    Second Commit

--- src/views/Pizza.jsx
+++ src/views/Pizza.jsx
@@ -1,16 +1,20 @@
 import { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import { PizzaContext } from '../context/PizzaProvider'
+import { Card } from 'react-bootstrap'
 
 const Pizza = () => {
   const { id } = useParams()
   const { pizzas } = useContext(PizzaContext)
+  console.log(pizzas)
 
-  const [pizzaDetail, setPizzaDetail] = useState([])
+  const [pizzaDetail, setPizzaDetail] = useState({})
   console.log(pizzaDetail)
 
   const data = () => {
     const pizzaData = pizzas.find((pizza) => pizza.id === id)
+    console.log(pizzaData)
+
     setPizzaDetail(pizzaData)
   }
 
@@ -18,7 +22,21 @@ const Pizza = () => {
     data()
   }, [])
 
-  return <div>{pizzaDetail}</div>
+  return (
+    <div>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img
+          variant='top'
+          src={pizzaDetail.img}
+        />
+        <Card.Body>
+          <Card.Title>{pizzaDetail.name}</Card.Title>
+          <Card.Text>{pizzaDetail.ingredients}</Card.Text>
+          <p>{pizzaDetail.price}</p>
+        </Card.Body>
+      </Card>
+    </div>
+  )
 }
 
 export default Pizza
